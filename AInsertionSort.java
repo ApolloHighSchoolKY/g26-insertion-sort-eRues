41dc8d8e74cafcc8268384611c1302a2d5b08e6b
eRues <ellie.rue@stu.daviess.kyschools.us>
2025-04-21 03:37:04 +0000

    finished, array algorithm functional

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,9 +1,28 @@
+import java.util.Arrays;
+
 public class AInsertionSort
 {
 
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
+        Integer temp = 0;
+
+        for(int i = 1; i < myNumbers.length; i++)
+        {
+            for(int j = i - 1; j > 0; j--)
+            {
+                if(myNumbers[i] < myNumbers[j])
+                {
+                    temp = myNumbers[j];
+                    myNumbers[j] = myNumbers[i];
+                    myNumbers[i] = temp;
+                    i--;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(myNumbers));
 
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ALInsertionSort
 {
@@ -10,24 +11,41 @@ public class ALInsertionSort
     	boolean inserted=false;
 
 		//Add the first number to the array list
+		sorted.add(myNumbers[0]);
 
 		//Loop once for all of the remaining numbers in the unsorted list
-
+		for(int i = 1; i < myNumbers.length; i++)
+		{
 			//Check with each of the numbers in the sorted list
-
+			for(int j = i - 1; j > 0; j--)
+			{
 				//If this number is less than one in the sorted list,
+				if(myNumbers[i] < myNumbers[j] && !inserted)
+				{
 				//insert it there
-
+					sorted.add(j, myNumbers[i]);
+					inserted = true;
+					
+				}
+			}
 			//If it was not inserted, stick it on the end.
-
+			if(inserted == false)
+			{
+				sorted.add(myNumbers[i]);
+			}
+			inserted = false;
 
 		//End Loop for unsorted list
-
-		System.out.println(sorted);
+		}
 
 		//Move the data back to the array
+		for(int i = 0; i < myNumbers.length; i++)
+		{
+			myNumbers[i] = sorted.get(i);
+		}
 
 		//Print the contents of the array
+		System.out.println(Arrays.toString(myNumbers));
 
     }
 
